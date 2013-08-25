625995178f164ba4b4ce7c816967599d06d95f8c
Jack Franklin <jack@jackfranklin.net>
2013-08-25 15:51:06 +0100

    tutorial5

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -6,3 +6,27 @@ var BookCollection = Backbone.Collection.extend({
   model: Book,
   url: "/books"
 });
+
+var BookView = Backbone.View.extend({
+  tagName: "li",
+  className: "book",
+  render: function() {
+    var template = $("#booktemplate").html();
+    var compiled = Handlebars.compile(template);
+    var html = compiled(this.model.attributes);
+    this.$el.html(html);
+    return this;
+  }
+});
+
+var BookCollectionView = Backbone.View.extend({
+  tagName: "ul",
+  className: "books",
+  render: function() {
+    this.collection.each(function(book) {
+      var bookView = new BookView({ model: book });
+      this.$el.append(bookView.render().el);
+    }, this);
+    return this;
+  }
+});
--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,3 +1,9 @@
 $(function() {
-  // some init
+  var collection = new BookCollection();
+  collection.fetch({
+    success: function(data) {
+      var view = new BookCollectionView({ collection: data });
+      $("body").append(view.render().el);
+    }
+  });
 });
